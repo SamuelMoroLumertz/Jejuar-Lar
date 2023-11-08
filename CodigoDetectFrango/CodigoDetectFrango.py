3e3005ea62a474ae9289dade06c1893b693feb16
William <william.silva147@outlook.com>
2023-11-08 14:02:44 -0300

    separando a classe galinha do codigo original

--- CodigoDetectFrango/CodigoDetectFrango.py
+++ CodigoDetectFrango/CodigoDetectFrango.py
@@ -8,29 +8,10 @@ from ultralytics import YOLO
 from collections import defaultdict
 import os
 import numpy as np
+from Galinha import Galinha
+
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
-class Galinha:
-    def __init__(self, galinha_id, pos_x, pos_y):
-        self.galinha_id = galinha_id
-        self.distancia_percorrida = 0
-        self.tempo_parado = 0
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-        self.tempo_comendo = 0
-        self.tempo_bebendo = 0
-
-    def atualizar_posicao(self, new_pos_x, new_pos_y):
-        # Calcula a distância percorrida desde a última atualização
-        distancia = ((new_pos_x - self.pos_x) ** 2 + (new_pos_y - self.pos_y) ** 2) ** 0.5
-        self.distancia_percorrida += distancia
-
-        # Atualiza a posição atual
-        self.pos_x = new_pos_x
-        self.pos_y = new_pos_y
-
-    def incrementar_tempo_parado(self):
-        self.tempo_parado += 1
 
 model = YOLO("C:/ImageDetection/ImageLabeling2/yolov8m_custom2.pt")
 video_path = "C:/ImageDetection/Segmentation1/Frango/hiv00001.mp4"
