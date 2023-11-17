4d3e8bd40661ec94d33eb8de0b92dc5df8a79103
Samuel Moro Lumertz <samuelmoro_l@hotmail.com>
2023-11-17 14:15:31 -0300

    Codigo da distorção da imagem do frango

--- CameraDistortion/calibration.py
+++ CameraDistortion/calibration.py
@@ -3,101 +3,108 @@ import cv2
 import glob
 import os
 
-chessBoardSize = (6,6)
-frameSize = (1280,720)
+#chessBoardSize = (6,6)
+#frameSize = (1280,720)
 
-def capture_photo():
-    cap = cv2.VideoCapture(1) 
-    photo_counter = 0
+#def capture_photo():
+#    cap = cv2.VideoCapture(1) 
+#    photo_counter = 0
 
-    while True:
-        ret, frame = cap.read() 
+#    while True:
+#        ret, frame = cap.read() 
         
-        if ret:
-            cv2.imshow('Webcam', frame)
+#        if ret:
+#            cv2.imshow('Webcam', frame)
 
-        key = cv2.waitKey(1)
+#        key = cv2.waitKey(1)
 
-        if key == ord('p'):  # Ao pressionar a letra 'p' do teclado, tira uma foto e a salva
-            photo_counter += 1
-            cv2.imwrite('imagens/chessboard{}.png'.format(photo_counter), frame)
-        elif key == ord('q'):  # Ao pressionar a letra 'q' do teclado, finaliza o processo
-            break
+#        if key == ord('p'):  # Ao pressionar a letra 'p' do teclado, tira uma foto e a salva
+#            photo_counter += 1
+#            cv2.imwrite('imagens/chessboard{}.png'.format(photo_counter), frame)
+#        elif key == ord('q'):  # Ao pressionar a letra 'q' do teclado, finaliza o processo
+#            break
 
-    cap.release()  # Libera a webcam
-    cv2.destroyAllWindows()
+#    cap.release()  # Libera a webcam
+#    cv2.destroyAllWindows()
 
-def calibration():
-    criteria = cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001
+#def calibration():
+#    criteria = cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001
 
-    objp = np.zeros((chessBoardSize[0]*chessBoardSize[1],3), np.float32)
-    objp[:,:2] = np.mgrid[0:chessBoardSize[0], 0:chessBoardSize[1]].T.reshape(-1,2)
+#    objp = np.zeros((chessBoardSize[0]*chessBoardSize[1],3), np.float32)
+#    objp[:,:2] = np.mgrid[0:chessBoardSize[0], 0:chessBoardSize[1]].T.reshape(-1,2)
 
     
-    objpoints = [] 
-    imgpoints = [] 
+#    objpoints = [] 
+#    imgpoints = [] 
 
-    images = glob.glob('imagens/camera320WS/*.jpg')
+#    images = glob.glob('imagens/camera320WS/*.jpg')
 
-    for image in images:
+#    for image in images:
         
-        img = cv2.imread(image)
-        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#        img = cv2.imread(image)
+#        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         
-        ret, corners = cv2.findChessboardCorners(gray, chessBoardSize, None)
+#        ret, corners = cv2.findChessboardCorners(gray, chessBoardSize, None)
         
-        if ret == True:
-            objpoints.append(objp)
+#        if ret == True:
+#            objpoints.append(objp)
 
-            corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-            imgpoints.append(corners2)
+#            corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+#            imgpoints.append(corners2)
 
-            img = cv2.drawChessboardCorners(img, chessBoardSize, corners2, ret)
-            cv2.imshow('img',img)
-            cv2.waitKey(500)
+#            img = cv2.drawChessboardCorners(img, chessBoardSize, corners2, ret)
+#            cv2.imshow('img',img)
+#            cv2.waitKey(500)
 
-    cv2.destroyAllWindows()
+#    cv2.destroyAllWindows()
 
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, frameSize, None, None)
+#    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, frameSize, None, None)
 
 
-    output_folder = 'imagens/camera320WS/Resultados'
+#    output_folder = 'imagens/camera320WS/Resultados'
 
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
+#    if not os.path.exists(output_folder):
+#        os.makedirs(output_folder)
 
-    for image_path in images:
-        img = cv2.imread(image_path)
-        h,  w = img.shape[:2]
-       # newCameraMtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+#    for image_path in images:
+#        img = cv2.imread(image_path)
+#        h,  w = img.shape[:2]
+#       # newCameraMtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
 
-        newCameraMtx = mtx
-        dst = cv2.undistort(img, mtx, dist, None, newCameraMtx)
+#        newCameraMtx = mtx
+#        dst = cv2.undistort(img, mtx, dist, None, newCameraMtx)
 
-        # dst = cv2.undistort(img, mtx, dist, None, newCameraMtx)
-        # x, y, w, h = roi
-        # dst = dst[y:y+h, x:x+w]
+#        # dst = cv2.undistort(img, mtx, dist, None, newCameraMtx)
+#        # x, y, w, h = roi
+#        # dst = dst[y:y+h, x:x+w]
 
-        output_path = os.path.join(output_folder, os.path.basename(image_path))
-        cv2.imwrite(output_path, dst)
+#        output_path = os.path.join(output_folder, os.path.basename(image_path))
+#        cv2.imwrite(output_path, dst)
 
-    output_file = 'imagens/camera320WS/Resultados/calibration_results.txt'
+#    output_file = 'imagens/camera320WS/Resultados/calibration_results.txt'
 
-    with open(output_file, 'w') as f:
-        f.write("Camera matrix:\n")
-        np.savetxt(f, mtx, fmt='%.8f')
-        f.write("\nDistortion coefficients:\n")
-        np.savetxt(f, dist, fmt='%.8f')
+#    with open(output_file, 'w') as f:
+#        f.write("Camera matrix:\n")
+#        np.savetxt(f, mtx, fmt='%.8f')
+#        f.write("\nDistortion coefficients:\n")
+#        np.savetxt(f, dist, fmt='%.8f')
 
-    # img = cv2.imread('imagens/cameraSteel/chessBoard.jpg')
-    # h,  w = img.shape[:2]
-    # newCameraMtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
-    #
-    # dst = cv2.undistort(img, mtx, dist, None, newCameraMtx)
-    # x, y, w, h = roi
-    # dst = dst[y:y+h, x:x+w]
-    # cv2.imwrite('imagens/resultados/resultado.jpg', dst)
+#    # img = cv2.imread('imagens/cameraSteel/chessBoard.jpg')
+#    # h,  w = img.shape[:2]
+#    # newCameraMtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+#    #
+#    # dst = cv2.undistort(img, mtx, dist, None, newCameraMtx)
+#    # x, y, w, h = roi
+#    # dst = dst[y:y+h, x:x+w]
+#    # cv2.imwrite('imagens/resultados/resultado.jpg', dst)
 
-#capture_photo()
-calibration()
+##capture_photo()
+#calibration()
 
+
+
+img = cv2.imread("C:/Users/samue/OneDrive/Imagens/Capturas de tela/Captura de tela 2023-11-16 132719.png")
+mtx= [834.94812252,0.00000000,637.95198468,0.00000000,834.43383649,302.29273117,0.00000000,0.00000000,1.00000000]
+dist=[-0.34945005,0.18133822,0.00095096,0.00142214,-0.12601488]
+dst = cv2.undistort(img, mtx, dist, None, mtx)
+cv2.imshow("saldkfjalksdjf", dst)
--- CodigoDetectFrango/CodigoSamuel/CodigoSamuel.py
+++ CodigoDetectFrango/CodigoSamuel/CodigoSamuel.py
@@ -7,98 +7,126 @@ import os
 import numpy as np
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE" #para evitar problemas de compatibilidade entre os ambientes
 
-model = YOLO("C:/ImageDetection/ImageLabeling2/yolov8m_custom2.pt") #carrega o modelo que sera usado para detectar as galinhas
-video_path = "C:/ImageDetection/Segmentation1/Frango/hiv00001.mp4"
-cap = cv2.VideoCapture(video_path)
-track_history = defaultdict(lambda: [])# Store the track history
-galinhas=[]
-contadorFrame=0
-distMax=20
+#model = YOLO("C:/ImageDetection/ImageLabeling2/yolov8m_custom2.pt") #carrega o modelo que sera usado para detectar as galinhas
+#video_path = "C:/ImageDetection/Segmentation1/Frango/hiv00001.mp4"
+#cap = cv2.VideoCapture(video_path)
+#track_history = defaultdict(lambda: [])# Store the track history
+#galinhas=[]
+#contadorFrame=0
+#distMax=20
 
-#criacao de uma classe GALINHA
-class Galinha:
-    def __init__(self, galinha_id, pos_x, pos_y):
-        self.galinha_id = galinha_id
-        self.distancia_percorrida = 0
-        self.tempo_parado = 0
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-        self.tempo_comendo = 0
-        self.tempo_bebendo = 0
+##criacao de uma classe GALINHA
+#class Galinha:
+#    def __init__(self, galinha_id, pos_x, pos_y):
+#        self.galinha_id = galinha_id
+#        self.distancia_percorrida = 0
+#        self.tempo_parado = 0
+#        self.pos_x = pos_x
+#        self.pos_y = pos_y
+#        self.tempo_comendo = 0
+#        self.tempo_bebendo = 0
 
-    def atualizar_posicao(self, new_pos_x, new_pos_y):
-        # Calcula a distancia percorrida desde a ultima atualizacao
-        distancia = ((new_pos_x - self.pos_x) ** 2 + (new_pos_y - self.pos_y) ** 2) ** 0.5
-        self.distancia_percorrida += distancia
-        # Atualiza a posicao atual
-        self.pos_x = new_pos_x
-        self.pos_y = new_pos_y
+#    def atualizar_posicao(self, new_pos_x, new_pos_y):
+#        # Calcula a distancia percorrida desde a ultima atualizacao
+#        distancia = ((new_pos_x - self.pos_x) ** 2 + (new_pos_y - self.pos_y) ** 2) ** 0.5
+#        self.distancia_percorrida += distancia
+#        # Atualiza a posicao atual
+#        self.pos_x = new_pos_x
+#        self.pos_y = new_pos_y
 
-    def incrementar_tempo_parado(self):
-        self.tempo_parado += 1
+#    def incrementar_tempo_parado(self):
+#        self.tempo_parado += 1
 
-def galinha_existe(galinhas, id_procurado):
-    for galinha in galinhas:
-        if galinha.galinha_id == id_procurado:
-            return galinha
-    return False
+#def galinha_existe(galinhas, id_procurado):
+#    for galinha in galinhas:
+#        if galinha.galinha_id == id_procurado:
+#            return galinha
+#    return False
 
-while True:
-    ret, frame = cap.read()
+#while True:
+#    ret, frame = cap.read()
 
-    if not ret:
-        break
+#    if not ret:
+#        break
 
-    contadorFrame=contadorFrame+1
+#    contadorFrame=contadorFrame+1
 
-    results = model.track(frame, persist=True, show_labels=False, show_conf=False)
-    # Get the boxes and track IDs
-    boxes = results[0].boxes.xywh.cpu()
-    track_ids = results[0].boxes.id.int().cpu().tolist()
-    print(len(track_ids))
-    for box, track_id in zip(boxes, track_ids):
-        x, y, w, h = box
-        x1, y1,x2, y2 = x-w/2, y-h/2, x+w/2, y+h/2
-        galinha = galinha_existe(galinhas, track_id)   
+#    results = model.track(frame, persist=True, show_labels=False, show_conf=False)
+#    # Get the boxes and track IDs
+#    boxes = results[0].boxes.xywh.cpu()
+#    track_ids = results[0].boxes.id.int().cpu().tolist()
+#    print(len(track_ids))
+#    for box, track_id in zip(boxes, track_ids):
+#        x, y, w, h = box
+#        x1, y1,x2, y2 = x-w/2, y-h/2, x+w/2, y+h/2
+#        galinha = galinha_existe(galinhas, track_id)   
 
-        if galinha is not False:
-            distancia = ((x - galinha.pos_x) ** 2 + (y - galinha.pos_y) ** 2) ** 0.5
-            if contadorFrame%30==0 and distancia >= distMax:
-                galinha.atualizar_posicao(x,y)
-                print(f'A galinha {galinha.galinha_id} se mexeu! Distancia: {galinha.distancia_percorrida}')
-            elif contadorFrame%30==0 and distancia < distMax:
-                galinha.incrementar_tempo_parado()
-        else:
-            novaGalinha = Galinha(track_id,x,y)
-            galinhas.append(novaGalinha)
+#        if galinha is not False:
+#            distancia = ((x - galinha.pos_x) ** 2 + (y - galinha.pos_y) ** 2) ** 0.5
+#            if contadorFrame%30==0 and distancia >= distMax:
+#                galinha.atualizar_posicao(x,y)
+#                print(f'A galinha {galinha.galinha_id} se mexeu! Distancia: {galinha.distancia_percorrida}')
+#            elif contadorFrame%30==0 and distancia < distMax:
+#                galinha.incrementar_tempo_parado()
+#                print(f'A galinha {galinha.galinha_id} ficou parada! Tempo Parado: {galinha.tempo_parado}')
+#        else:
+#            novaGalinha = Galinha(track_id,x,y)
+#            galinhas.append(novaGalinha)
 
-        cv2.rectangle(frame, (int(x1),int(y1)),(int(x2),int(y2)), (0, 0, 255), 1)
-        t_size = cv2.getTextSize(str(track_id), 0, fontScale=1 / 1, thickness=1)[0]
-        cv2.rectangle(frame, (int(x1), int(y1) - t_size[1] - 3),(int(x1) + t_size[0], int(y1)+3),  (0,255,0),-1)
-        cv2.putText(frame, str(track_id), (int(x1), int(y1) - 2), 0, 1 / 1,[0,0,0], thickness=2, lineType=cv2.LINE_AA)
-        track = track_history[track_id]
-        track.append((float(x), float(y))) 
+#        cv2.rectangle(frame, (int(x1),int(y1)),(int(x2),int(y2)), (0, 0, 255), 1)
+#        t_size = cv2.getTextSize(str(track_id), 0, fontScale=1 / 1, thickness=1)[0]
+#        cv2.rectangle(frame, (int(x1), int(y1) - t_size[1] - 3),(int(x1) + t_size[0], int(y1)+3),  (0,255,0),-1)
+#        cv2.putText(frame, str(track_id), (int(x1), int(y1) - 2), 0, 1 / 1,[0,0,0], thickness=2, lineType=cv2.LINE_AA)
+#        track = track_history[track_id]
+#        track.append((float(x), float(y))) 
         
-        if len(track) > 30: 
-            track.pop(0)
+#        if len(track) > 30: 
+#            track.pop(0)
 
-        points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2)) 
-        cv2.polylines(frame, [points], isClosed=False, color=(230, 0, 0), thickness=5)
+#        points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2)) 
+#        cv2.polylines(frame, [points], isClosed=False, color=(230, 0, 0), thickness=5)
 
-    cv2.imshow("Detections", frame)
+#    cv2.imshow("Detections", frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+#    if cv2.waitKey(1) & 0xFF == ord('q'):
+#        break
 
-cap.release()
+#cap.release()
+#cv2.destroyAllWindows()
+
+####CLASSE DOS FRANGOS
+##ID
+##DISTPERCORRIDA
+##TEMPOPARADO
+##POSX
+##POSY
+##TEMPOCOMENDO
+##TEMPOBEBENDO
+
+
+
+
+
+
+
+
+
+img = cv2.imread("C:/Users/samue/OneDrive/Imagens/Capturas de tela/Captura de tela 2023-11-16 132719.png")
+resized = cv2.resize(img, (1280,720), interpolation=cv2.INTER_AREA)
+mtx = [
+    [834.94812252, 0.00000000, 637.95198468],
+    [0.00000000, 834.43383649, 302.29273117],
+    [0.00000000, 0.00000000, 1.00000000]
+]
+
+dist = [-0.34945005, 0.18133822, 0.00095096, 0.00142214, -0.12601488]
+
+dst = cv2.undistort(resized, np.array(mtx), np.array(dist), None, np.array(mtx))
+dst2 = cv2.resize(dst, (1920,1080), interpolation=cv2.INTER_AREA)
+
+cv2.namedWindow("resized", cv2.WINDOW_NORMAL)
+cv2.imshow("resized", dst)
+cv2.namedWindow("normal", cv2.WINDOW_NORMAL)
+cv2.imshow("normal", dst2)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-###CLASSE DOS FRANGOS
-#ID
-#DISTPERCORRIDA
-#TEMPOPARADO
-#POSX
-#POSY
-#TEMPOCOMENDO
-#TEMPOBEBENDO
-
