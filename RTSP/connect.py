e012d5f23c85864721dda488c5f0db4119005c8b
William <william.silva147@outlook.com>
2023-12-07 14:23:47 -0300

    Conectando as cameras usando RTSP

---
+++ RTSP/connect.py
@@ -0,0 +1,43 @@
+import cv2
+import tkinter as tk
+
+# RTSP URL of the video streams
+cameraDireita = "rtsp://camlab14b:senhasegura@10.57.1.19:554/stream1"
+cameraEsquerda = "rtsp://camlab14b:senhasegura@10.57.1.20:554/stream2"
+cameraAtual = cameraEsquerda
+
+janela = (1024, 768)
+video = cv2.VideoCapture(cameraAtual)
+
+# Get screen size
+root = tk.Tk()
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+root.destroy()
+
+while True:
+    ret, frame = video.read()
+
+    frame = cv2.resize(frame, janela)
+    cv2.imshow("RTSP", frame)
+
+    # Calculate the position to center the window
+    x = int((screen_width - janela[0]) / 2)
+    y = int((screen_height - janela[1]) / 2)
+
+    # Move the window to the center of the screen
+    cv2.moveWindow("RTSP", x, y)
+
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+    elif key == ord('t'):
+        if cameraAtual == cameraEsquerda:
+            cameraAtual = cameraDireita
+        else:
+            current_camera = cameraEsquerda
+        video.release()
+        video = cv2.VideoCapture(cameraAtual)
+
+video.release()
+cv2.destroyAllWindows()
